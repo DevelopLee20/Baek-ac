a8014571dd284a6aa62c08ea1345725c6b804acf
LeeInGyu <developLee20@gmail.com>
2024-03-10 17:57:10 +0900

    2024-03-10 baekjoon

--- 2000/Num_2751.py
+++ 2000/Num_2751.py
@@ -1,16 +1,29 @@
-lst = []
-count = int(input())
+'''
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
 
-for _ in range(count):
-    lst.append(int(input()))
-   
-# Bubble 
-for _ in range(count):
-    for j in range(1, count):
-        if lst[j-1] < lst[j]:
-            swap = lst[j-1]
-            lst[j-1] = lst[j]
-            lst[j] = swap
-            
-for i in range(count):
-    print(lst[i])
+5
+5
+4
+3
+2
+1
+
+1
+2
+3
+4
+5
+
+sort 함수는 원 배열을 수정
+sorted 함수는 원 배열을 수정하지 않는다
+'''
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+lst = [int(input()) for i in range(N)]
+
+for i in sorted(lst):
+    print(i)
