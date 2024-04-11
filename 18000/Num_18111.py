94b77fcd68d346232710ac873d9793572e8a951f
LeeInGyu <developlee20@gmail.com>
2024-04-11 15:06:28 +0900

    백준 18111번 문제풀기, 좌표가 의미 없을 때 튜플로 변환하는 방법도 좋다

--- 18000/Num_18111.py
+++ 18000/Num_18111.py
@@ -1,25 +1,36 @@
-'''
-N: 집터의 세로
-M: 집터의 가로
-B: 초기 블록 개수
-
-땅의 높이 0~256
-최소 시간을 가지는 것이 답이 여러 개라면 땅의 높이가 가장 높은 것
-
-행동1: 블록 제거해서 인벤토리 넣기 -> 2초
-행동2: 블록 꺼내서 놓기 -> 1초
-
-입력
-N, M, B
-board
-'''
 import sys
 
 input = sys.stdin.readline
 
 N, M, B = map(int, input().split())
 
-board = []
+board = {}
+BlockSum = B
 for _ in range(N):
-    board.append([int(i) for i in input().rstrip().split()])
+    for i in [int(i) for i in input().rstrip().split()]:
+        BlockSum += i
+        if i in board:
+            board[i] += 1
+        else:
+            board[i] = 1
 
+MaxHeight = int(BlockSum / (N*M))
+ResultSec = 2 * 256 * N * M + 1; ResultHeight = -1
+
+for TargetHeight in range(0, MaxHeight+1):
+    sec = 0
+    for height, count in board.items():
+        gap = TargetHeight - height
+
+        if gap > 0:
+            sec += gap * count
+        elif gap < 0:
+            sec += gap * count * -2
+
+    if ResultSec > sec:
+        ResultHeight = TargetHeight
+        ResultSec = sec
+    elif ResultSec == sec and ResultHeight < TargetHeight:
+        ResultHeight = TargetHeight
+
+print(ResultSec, ResultHeight)
