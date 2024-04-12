d5856a5e41cb5e65bb818cdd6a11e9e0771a569d
LeeInGyu <developlee20@gmail.com>
2024-04-12 10:51:22 +0900

    1003번, 패턴이 보이는 점화식은 다이나믹 프로그래밍으로

--- 1000/Num_1003.py
+++ 1000/Num_1003.py
@@ -1,20 +1,26 @@
-# 시간초과 : 다른 알고리즘으로 찾아야겠다, 다이나믹??? 나중에 다시 온다.
+'''
+나온대로 작성
+T 케이스 개수
+N -> T개 만큼 출력
+
+
+출력 -> (0 호출 횟수) (1 호출 횟수)
+
+'''
 
 import sys
 
 input = sys.stdin.readline
 
-def fibonacci(n:int, count:list):
-    if n == 0:
-        count[n] += 1
-        return 0
-    elif n == 1:
-        count[n] += 1
-        return 1
-    else:
-        return fibonacci(n-1, count) + fibonacci(n-2, count)
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+
+    zero = 1; one = 0
+    for __ in range(N):
+        next = zero + one
+        zero = one
+        one = next
     
-for _ in range(int(input())):
-    count = [0,0]
-    fibonacci(int(input()), count)
-    print(*count)
+    print(zero, one)
