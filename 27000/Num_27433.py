0e2ebf1204c641ad133dae0f775ca42cd4821f00
LeeInGyu <developlee20@gmail.com>
2024-04-15 13:58:07 +0900

    브론즈 5 문제 탐방기, 휴식중

---
+++ 27000/Num_27433.py
@@ -0,0 +1,8 @@
+N = int(input())
+
+Sum = 1
+
+for i in range(2, N+1):
+    Sum *= i
+
+print(Sum)
---
+++ 4000/Num_4999.py
@@ -0,0 +1,7 @@
+JaeLen = input()
+DockerLen = input()
+
+if len(JaeLen) >= len(DockerLen):
+    print("go")
+else:
+    print("no")
