663d25a7447e9a59b178247f1b97b04ba4a7ca2f
LeeInGyu <developLee20@gmail.com>
2024-05-14 22:02:25 +0900

    1389 풀기, BFS 방식을 외우고, 수식을 잘 구현해보자

--- 1000/Num_1389.py
+++ 1000/Num_1389.py
@@ -11,9 +11,39 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 graph = [[0 for i in range(N)] for i in range(N)]
-for _ in range(N):
+for _ in range(M):
     x, y = map(int, input().split())
     graph[x-1][y-1] = 1 # idx라서 -1 해준다.
+    graph[y-1][x-1] = 1 # idx라서 -1 해준다.
 
-for i in range(N):
-    
+resultGraph = [[0 for i in range(N)] for i in range(N)]
+for startIdx in range(N):
+    visited = [0 for i in range(N)]
+    queue = [startIdx]
+    visited[startIdx] = True
+    count = 1
+    addCount = 1
+    newCount = 0
+
+    while len(queue) > 0:
+        edge = queue.pop(0)
+        addCount -= 1
+
+        for idx, connect in enumerate(graph[edge]):
+            if connect and visited[idx] == 0:
+                visited[idx] = True
+                resultGraph[startIdx][idx] = count
+                queue.append(idx)
+                newCount += 1
+
+        if addCount == 0:
+            addCount = newCount
+            newCount = 0
+            count += 1
+
+minIdx = 0
+for idx in range(1, N):
+    if sum(resultGraph[minIdx]) > sum(resultGraph[idx]):
+        minIdx = idx
+
+print(minIdx+1)
