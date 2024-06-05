a87af2021a7c0a6512f2bd14386e1e1f4bd4ddb5
LeeInGyu <developLee20@gmail.com>
2024-06-05 21:59:06 +0900

    2024-06-05 11726, dp는 점화식처럼 쓰기만 하면 되나

--- 11000/Num_11726.py
+++ 11000/Num_11726.py
@@ -1,3 +1,9 @@
 n = int(input())
+dp = [0] * max(3, n+1)
+dp[1] = 1
+dp[2] = 2
 
-print(2**(int(n/2)) + int(n % 2))
+for idx in range(3, n+1):
+    dp[idx] = (dp[idx-1] + dp[idx-2]) % 10007
+
+print(dp[n])
