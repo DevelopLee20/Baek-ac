5ea5fcc96248fafbbe13d73504221efd9693bfbc
LeeInGyu <developLee20@gmail.com>
2024-06-15 18:51:46 +0900

    modified test.py

--- test.py
+++ test.py
@@ -1,23 +1,11 @@
-first = input()
-second = input()
-three = input()
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         sum <- sum + A[i]; # 코드1
+#     return sum;
+# }
 
-colorList = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
+n = int(input())
 
-R = ''
-for idx in range(10):
-    if first == colorList[idx]:
-        R += str(idx)
-        break
-
-for idx in range(10):
-    if second == colorList[idx]:
-        R += str(idx)
-        break
-
-for idx in range(10):
-    if three == colorList[idx]:
-        R = int(R) * (10**idx)
-        break
-
-print(R)
+print(n)
+print(1)
