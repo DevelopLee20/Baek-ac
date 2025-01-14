3f78718eb947107a324703e4dc79c122d19bb913
LeeInGyu <developLee20@gmail.com>
2025-01-14 18:51:09 +0900

    2024-01-14 상처뿐인 DP

--- 17000/Num_17626.py
+++ 17000/Num_17626.py
@@ -1,10 +1,19 @@
 n = int(input())
 
-for x1 in range(int(n**0.5) + 1):
-    for x2 in range(int((n-x1*x1)**0.5) + 1):
-        for x3 in range(int((n-x1*x1-x2*x2)**0.5) + 1):
-            for x4 in range(int((n-x1*x1-x2*x2-x3*x3)**0.5) + 1):
-                if x4 > 0:
-                    if x1**2 + x2**2 + x3**2 + x4**2 == n:
-                        print((x1 != 0) + (x2 != 0) + (x3 != 0) + (x4 != 0))
-                        exit(0)
+array = [4] * (n+1)
+square = [j*j for j in range(1, int(n**0.5)+1)]
+
+for i in square:
+    array[i] = 1
+
+for i in range(2, n+1):
+    if array[i] == 1:
+        continue
+
+    for j in square:
+        if j > i:
+            break
+
+        array[i] = min(array[i], array[i - j] + 1)
+
+print(array[n])
