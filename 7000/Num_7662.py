34a5b8784927ecc51c1938f085d6770f12add8fc
LeeInGyu <62877861+DevelopLee20@users.noreply.github.com>
2025-02-22 22:51:44 +0900

    2025-02-22 게으른 삭제, lazy deletion 체크만 해두고 나중에 삭제 처리(효율을 위해서)

--- 7000/Num_7662.py
+++ 7000/Num_7662.py
@@ -4,31 +4,41 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
-for t in range(T):
+for _ in range(T):
     k = int(input())
+
     heapMax = []
     heapMin = []
-
-    for _ in range(k):
+    visited = [False] * k
+    for index in range(k):
         key, value = input().split()
         value = int(value)
 
         if key == "I":
-            heapq.heappush(heapMax, -value)
-            heapq.heappush(heapMin, value)
-        # elif key == "D":
-        else:
-            if len(heap) > 0:
-                if value == -1:
-                    heapq.heappop(heap)
-                # elif value == 1:
-                else:
-                    heap = [heapq.heappop(heap) for _ in range(len(heap))]
-                    heap.pop()
+            heapq.heappush(heapMax, (-value, index))
+            heapq.heappush(heapMin, (value, index))
+            visited[index] = True
+        elif key == "D":
+            if value == -1:
+                while heapMin and not visited[heapMin[0][1]]:
+                    heapq.heappop(heapMin)
+                if heapMin:
+                    visited[heapMin[0][1]] = False
+                    heapq.heappop(heapMin)
+            
+            elif value == 1:
+                while heapMax and not visited[heapMax[0][1]]:
+                    heapq.heappop(heapMax)
+                if heapMax:
+                    visited[heapMax[0][1]] = False
+                    heapq.heappop(heapMax)
     
-    heap = [heapq.heappop(heap) for _ in range(len(heap))]
+    while heapMax and not visited[heapMax[0][1]]:
+        heapq.heappop(heapMax)
+    while heapMin and not visited[heapMin[0][1]]:
+        heapq.heappop(heapMin)
 
-    if len(heap) == 0:
+    if not heapMin or not heapMax:
         print("EMPTY")
     else:
-        print(heap[-1], heap[0])
+        print(-heapMax[0][0], heapMin[0][0])
